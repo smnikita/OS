8815b59c5fcb796a88e99224e7b15c8246d09797
nik <smnikita@list.ru>
2015-04-13 18:48:00 +0300

    buf fix

--- lib/bufio.c
+++ lib/bufio.c
@@ -1,7 +1,7 @@
 #include "bufio.h"
 
 struct buf_t *buf_new(size_t capacity) {
-    struct buf_t *p = malloc(2 * sizeof(size_t) + capacity + sizeof(char *));
+    struct buf_t *p = malloc(2 * sizeof(size_t) + capacity);    
     p->capacity = capacity;
     p->size = 0;
     return p;
@@ -13,29 +13,23 @@ void buf_free(struct buf_t * p) {
 
 size_t buf_capacity(struct buf_t * p) {
     #ifdef DEBUG
-        if (buf == NULL) {
-            abort();
-        }
+        if (buf == NULL) abort();
     #endif
     return p->capacity;
 }
 
 size_t buf_size(struct buf_t * p) {
     #ifdef DEBUG
-        if (buf == NULL) {
-            abort();
-        }
+        if (buf == NULL) abort();        
     #endif
     return p->size;
 }
 
 ssize_t buf_fill(fd_t fd, struct buf_t *buf, size_t required) {
     #ifdef DEBUG
-        if (buf == NULL || buf->capacity < required) {
-            abort();
-        }
+        if (buf == NULL || buf->capacity < required) abort();        
     #endif
-    char* buffer = buf->buffer;
+    char* buffer = (char*)buf + 2 * sizeof(size_t);
     ssize_t read_size;
     while (1) {
         read_size = read(fd, buffer + buf->size, required - buf->size);
@@ -51,26 +45,22 @@ ssize_t buf_fill(fd_t fd, struct buf_t *buf, size_t required) {
 
 ssize_t buf_flush(fd_t fd, struct buf_t *buf, size_t required) {
     #ifdef DEBUG
-        if (buf == NULL) {
-            abort();
-        }
+        if (buf == NULL) abort();        
     #endif
-    char* buffer = buf->buffer;
-    ssize_t write_size;
-    ssize_t written = 0;
+    char* buffer = (char*)buf + 2 * sizeof(size_t);    
+    ssize_t written_cnt = 0;
     while (1) {
-        write_size = write(fd, buffer + written, buf->size - written);
-        if (write_size == -1) {
-            return -1;
-        }
-        written += write_size;
-        if (written >= required || buf->size == written) {
+        ssize_t write_size = write(fd, buffer + written_cnt, buf->size - written_cnt);
+        if (write_size == -1) return -1;        
+        written_cnt += write_size;
+        if (written_cnt >= required || buf->size == written_cnt) {
             int i;
-            for (i = 0; i < buf->size - written; i++) {
-                buf[i] = buf[i + written];
+            for (i = 0; i < buf->size - written_cnt; i++) {
+                buf[i] = buf[i + written_cnt];
             }
-            buf->size -= written;
-            return written;
+            buf->size -= written_cnt;
+            return written_cnt;
         }
     }
 }
+
--- lib/bufio.h
+++ lib/bufio.h
@@ -8,8 +8,7 @@
 
 struct buf_t {
 	size_t capacity;
-    size_t size;
-    char* buffer;
+    size_t size;    
 };
 
 struct buf_t *buf_new(size_t capacity);
@@ -18,5 +17,8 @@ size_t buf_capacity(struct buf_t *);
 size_t buf_size(struct buf_t *);
 ssize_t buf_fill(fd_t fd, struct buf_t *buf, size_t required);
 ssize_t buf_flush(fd_t fd, struct buf_t *buf, size_t required);
+
+ssize_t buf_getline(fd_t fd, struct buf_t *buf, char *dest);
+ssize_t buf_write(fd_t fd, struct buf_t *buf, char *src, size_t len);
 
 #endif
